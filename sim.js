a9cec5ef99745eddc196fb53e7dfe2bef9a6553c
haleema <isahalima18@gmail.com>
2025-01-21 23:35:55 +0100

    Updated code and added new files

--- sim.js
+++ sim.js
@@ -1,15 +1,15 @@
-function Account(nmae, balance) {
-  this.name ="name";
-  this.balance ="balance";  
+function Account(name, balance) {
+  this.name =name;
+  this.balance =balance;  
 }
 
 Account.prototype.deposit=function(amount){
     if (amount>0){
         this.balance +=amount;
-    console.info('Deposit: $(this.name) new balance is $${this.balance}');
+        console.log(`Deposit: ${this.name} new balance is $${this.balance}`);
         return true;
     } else{
-        console.error("Deposit amount must be positive");
+        console.log("Deposit amount must be positive");
          return false;
     }
 }
@@ -17,13 +17,13 @@ Account.prototype.deposit=function(amount){
 Account.prototype.withdraw = function(amount) {
     if (amount > 0 && amount <= this.balance) {  
         this.balance -= amount;
-        console.info(`Withdraw: ${this.name}'s new balance is $${this.balance}`);
+        console.log(`Withdraw: ${this.name} new balance is $${this.balance}`);
         return true;
     } else if (amount > this.balance) {
-        console.error("Withdrawal denied: Insufficient funds!");
+        console.log("Withdrawal denied: Insufficient funds!");
         return false;
     } else {
-        console.error("Withdrawal amount must be positive!");
+        console.log("Withdrawal amount must be positive!");
         return false;
     }
 };
